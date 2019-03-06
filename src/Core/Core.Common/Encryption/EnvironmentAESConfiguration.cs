ad344adf55fa9d21867f0d4181498ff612213df4
Arthur Correa <acorrea@athenahealth.com>
2019-03-06 11:40:58 -0500

    baseline a true common project for my stuff

---
+++ src/Core/Core.Common/Encryption/EnvironmentAESConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PucksAndProgramming.Common.Encryption
+{
+    public class EnvironmentAESConfiguration : AESConfiguration
+    {
+        public EnvironmentAESConfiguration()
+        {
+            this.EncryptionKey = Environment.GetEnvironmentVariable("AES_ENCRYPTION_KEY");
+            this.Salt = Environment.GetEnvironmentVariable("AES_ENCRYPTION_SALT");
+        }
+    }
+}
---
+++ src/Core/Core.Common/Utilities/LogManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Extensions.Logging;
+
+namespace PucksAndProgramming.Common.Utilities
+{
+    public static class LogManager
+    {
+        public static ILoggerFactory LoggerFactory { get; } = new LoggerFactory();
+
+        public static ILogger CreateLogger<T>()
+        {
+            return LoggerFactory.CreateLogger<T>();
+        }
+    }
+}
